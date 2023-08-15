887ca4213d9acd9906752653a946baf6846826e7
VincentFong <47265000+newVincentFong@users.noreply.github.com>
2023-08-15 11:24:10 +0800

    Set up vue library

---
+++ packages/virtual-scroll-list-vue/vite.config.ts
@@ -0,0 +1,26 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+const name = 'virtual-scroll-list-vue'
+
+const outputFileName = {
+  es: `${name}.mjs`,
+  cjs: `${name}.cjs`,
+  iife: `${name}.iife.js`,
+}
+
+const formats = Object.keys(outputFileName) as Array<keyof typeof outputFileName>
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VirtualScrollListVue',
+      formats,
+      fileName: (format) => outputFileName[format],
+    }
+  }
+})
